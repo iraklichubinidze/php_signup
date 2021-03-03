e3b34a19c60fa250f981e9148047df63470a6830
Irakli Chubinidze <ikchubinidze18@gmail.com>
2021-03-03 18:25:19 +0400

    Updated 2.2

--- mail.php
+++ mail.php
@@ -7,8 +7,40 @@ if (isset($_POST['submit'])) {
 	$errorEmpty = false;
 	$errorEmail = false;
 
-	if (empty($name)) {
-		echo "<span>Fill in all the fields!</span>";
+	if (empty($name) || empty($surname) || empty($email)) {
+		echo "<span class='form-error'>Fill in all the fields!</span>";
+		$errorEmpty = true;
+	}
+	elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		echo "<span class='form-error'>Write a valid e-mail adress ! </span>";
+		$errorEmail = true;
+	}
+	else {
+		echo "<span class='form-success'>Everything OK !</span>";
 	}
 }
+else {
+	echo "There was an error!"
+}
 ?>
+<script>
+	$("#mail-name, #mail-email").removeClass("input-error");
+
+	var errorEmpty = "<?php echo $errorEmpty; ?>";
+	var errorEmail = "<?php echo $errorEmail; ?>";
+
+	if(errorEmpty == true){
+		$("#mail-name, #mail-email").addClass("input-error");
+	}
+	if(errorMail == true){
+		$("#mail-email").addClass("input-error");
+	}
+	if (errorEmpty==false && errorMail==false){
+		$("#mail-name, #mail-email").val("");
+	}
+</script>
+
+
+
+
+
