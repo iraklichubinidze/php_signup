6f54b823043217454a7df30fb9b9c2a057d0853f
Irakli Chubinidze <ikchubinidze18@gmail.com>
2021-03-04 18:42:30 +0400

    Update 2.7

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<?php
+    require_once('config.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up Form</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/fingerprint-24px.svg">
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         $(document).ready(function(){
@@ -26,18 +31,40 @@
 </head>
 
 <body>
+<div>
+    <?php
+        if(isset($_POST['submit'])){
+            $firstname = $_POST['firstname'];
+            $lastname = $_POST['lastname'];
+            $email = $_POST['email'];
+
+            $sql = "INSERT INTO users (firstname,lastname,email) VALUES(?,?,?)";
+            $stmtinsert = $db->prepare($sql);
+            $result = $stmtinsert->execute([$firstname,$lastname,$email]);
+            if($result){
+                echo 'Successfully saved !';
+            }
+            else {
+                echo 'There were errors while saving the data';
+            }
+        }
+
+    ?>
+</div>
+
+
+<form action="mail.php" method="POST">
 <div id="login-box">
     <div class="left">
-        <form action="mail.php" method="POST">
-            <h1>Sign up form</h1> 
-            <input  id="mail-name" type="text" name="firstname" placeholder="First Name" />
-            <input  id="mail-lastname" type="text" name="lastname" placeholder="Last Name" />
-            <input  id="mail-email" type="text" name="email" placeholder="E-mail" />
-            <input id="mail-submit" type="submit" name="submit" value="Sign me up" />
+            <h1>Sign up form</h1>
+            <input  required for="firstname" id="mail-name" type="text" name="firstname" placeholder="First Name" />
+            <input  required for="lastname" id="mail-lastname" type="text" name="lastname" placeholder="Last Name" />
+            <input  required for="email" id="mail-email" type="text" name="email" placeholder="E-mail" />
+            <input  required id="mail-submit" type="submit" name="submit" value="Sign me up" />
             <p class="form-message"></p>
-        </form>
     </div>
 </div>
+</form>
 
 </body>
 </html>
