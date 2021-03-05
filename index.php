d5188c448f03498a280cbb45bf2870727c56a1e5
Irakli Chubinidze <ikchubinidze18@gmail.com>
2021-03-05 23:27:39 +0400

    Updated 2.x

--- index.php
+++ index.php
@@ -11,9 +11,48 @@
     <link rel="stylesheet" href="style/reset.css">
     <link rel="stylesheet" href="style/style.css">
 </head>
-
 <body>
-<div>
+    <?php 
+        $firstnameErr = $lastnameErr = $emailErr = "";
+        $firstname2 = $lastname2 = $email2 = "";
+        if($_SERVER['REQUEST_METHOD']=="POST"){
+            if(empty($_POST['firstname2'])) {
+                $firstnameErr = "Firstname is required ! ";
+            }else {
+                $firstname2 = test_input($_POST['firstname2']);
+                if(!preg_match("/^[a-zA-Z ]*$/",$firstname2)){
+                    $firstnameErr = "Only letters and spaces are allowed ! ";
+                }
+            }
+            if(empty($_POST['lastname2'])) {
+                $lastname2 = "Lastname is required ! ";
+            }else {
+                $lastname2 = test_input($_POST['lastname2']);
+                if(!preg_match("/^[a-zA-Z ]*$/",$lastname2)){
+                    $lastnameErr = "Only letters and spaces are allowed ! ";
+                }
+            }
+            
+            if(empty($_POST['email2'])) {
+                $emailErr = 'Email adress is required ! ';
+            } else {
+                $email2 = test_input($_POST['email2']);
+                if(!filter_var($email2,FILTER_VALIDATE_EMAIL)){
+                    $emailErr = "Email format is incorrect ! ";
+                }
+            }
+        }    
+    
+            function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+    ?>
+
+
+
     <?php
         if(isset($_POST['create'])){
             $firstname = $_POST['firstname'];
@@ -23,36 +62,36 @@
             $sql = "INSERT INTO users (firstname,lastname,email) VALUES(?,?,?)";
             $stmtinsert = $db->prepare($sql);
             $result = $stmtinsert->execute([$firstname,$lastname,$email]);
-           
         }
     ?>
-</div>
 
 
-<form action="index.php" method="post">
 <div id="login-box">
     <div class="left">
             <h1>Sign up form</h1>
-<!--            <input  required for="firstname" id="mail-name" type="text" name="firstname" placeholder="First Name" />-->
-<!--            <input  required for="lastname" id="mail-lastname" type="text" name="lastname" placeholder="Last Name" />-->
-<!--            <input  required for="email" id="mail-email" type="text" name="email" placeholder="E-mail" />-->
-<!--            <input  required id="mail-submit" type="submit" name="submit" value="Sign me up" />-->
-            <label for="firstname"><b>FIrst name</b></label>
-            <input class="firstname-j" id="mail-name" type="text" name="firstname" required>
+            <p><span class="error">* Required field</span></p>
+            <br> 
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 
-            <label for="lastname"><b>Last name</b></label>
-            <input class="lastname-j" id="mail-lastname" type="text" name="lastname" required>
+                <label for="firstname"><b>FIrst name</b></label>
+                <span class="error">* <?php echo $firstnameErr; ?> </span>
+                <input value="<?php echo $firstname2; ?>" class="firstname-j" id="mail-name" type="text" name="firstname" required>
 
-            <label for="email"><b>Email Address</b></label>
-            <input class="email-j" id="mail-email" type="email" name="email" required>
 
-            <input class="submit-j" type="submit" id="mail-submit" name="create" value="Sign Up">
+                <label for="lastname"><b>Last name</b></label>
+                <span class="error">*<?php echo $lastnameErr; ?> </span>
+                <input value="<?php echo $lastname2; ?>" class="lastname-j" id="mail-lastname" type="text" name="lastname" required>
+
+                <label for="email"><b>Email Address</b></label>
+                <span class="error">*<?php echo $emailErr; ?> </span>
+                <input value="<?php echo $email2; ?>" class="email-j" id="mail-email" type="text" name="email" required>
             
 
-            <p class="form-message"></p>
+            <input class="submit-j" type="submit" id="mail-submit" name="create" value="Sign Up">
+            </form>            
     </div>
 </div>
-</form>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 <script type="text/javascript">
