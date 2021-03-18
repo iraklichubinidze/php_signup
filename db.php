49047165ae2dc1ad64aa298ead3a1f9ee7c68b0c
System Administrator <root@MacBook-Air.local>
2021-03-18 20:04:42 +0400

    Updated

--- db.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'constants.php';
-
-$conn = new msqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once 'authController.php';
+    require_once('config.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,23 +11,47 @@
     <link rel="stylesheet" href="style/style1.css">
 </head>
 <body>
+
+
+    <div>
+        <?php
+            if(isset($_POST['submit'])){
+                $firstname = $_POST['firstname'];
+                $lastname = $_POST['lastname'];
+                $email = $_POST['email'];
+
+                $sql = "INSERT INTO users (email,firstname,lastname) VALUES(?,?,?)";
+                $stmtinsert = $db->prepare($sql);
+                $result = $stmtinsert->execute([$email,$firstname,$lastname]);
+                if($result){
+                    echo "success";
+                }else {
+                    echo "there were errors while saving the data";
+                }
+            }
+        ?>
+    </div>
+
     <div class="form-1">
         <form method="post" action="index.php" class="form">
+
             <h1 class="form_title">Sign up form</h1>
 
             <div class="form_div">
-                    <input type="email" name="mail" class="form_input" placeholder=" " required>
-                    <label for="" class="form_label">Email</label>
+                    <input type="email" name="email" class="form_input" placeholder=" " required>
+                    <label for="email" class="form_label">Email</label>
             </div>
+
             <div class="form_div">
-                <input type="text" name="firstname" class="form_input" placeholder=" ">
-                <label for="" class="form_label">First name</label>
+                    <input type="text" name="firstname" class="form_input" placeholder=" " required>
+                    <label for="" class="form_label">First name</label>
             </div>
+
             <div class="form_div">
-                <input type="text" name="lastname" class="form_input" placeholder=" ">
-                <label for="" class="form_label">Last name</label>
+                    <input type="text" name="lastname" class="form_input" placeholder=" " required>
+                    <label for="" class="form_label">Last name</label>
             </div>
-            <input class="form_button" name="submit" value="Sign Up" type="submit">
+                    <input class="form_button" name="submit" value="Sign Up" type="submit">
             </form>
     </div>
 
